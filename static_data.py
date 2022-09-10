e23f2a10e616da91e12d47eecf03cc6d011311b8
einsteinNjoroge <forensicslabnet@gmail.com>
2022-09-09 21:55:18 -0700

    Add static data

---
+++ static_data.py
@@ -0,0 +1,86 @@
+industries = [
+    'Manufacturing',
+    'Education',
+    'Finance',
+    'Technology',
+    'Agriculture',
+]
+
+chart_data = [
+    {
+        "Year": "2013",
+        "Manufacturing": 165,
+        "Education": 1,
+        "Finance": 134,
+        "Technology": 8,
+        "Agriculture": 37
+    },
+    {
+        "Year": "2014",
+        "Manufacturing": 60,
+        "Education": 123,
+        "Finance": 46,
+        "Technology": 44,
+        "Agriculture": 54
+    },
+    {
+        "Year": "2015",
+        "Manufacturing": 70,
+        "Education": 26,
+        "Finance": 67,
+        "Technology": 99,
+        "Agriculture": 52
+    },
+    {
+        "Year": "2016",
+        "Manufacturing": 116,
+        "Education": 181,
+        "Finance": 35,
+        "Technology": 112,
+        "Agriculture": 188
+    },
+    {
+        "Year": "2017",
+        "Manufacturing": 130,
+        "Education": 10,
+        "Finance": 86,
+        "Technology": 40,
+        "Agriculture": 72
+    }
+]
+
+comments = {
+    'Manufacturing': {
+        "2013": [
+            {
+                "id": "7d111618-d287-42a2-8331-f4c907e5c5b6",
+                "username": "Einstein",
+                "message": "Comment in Manufacturing 2013"
+            },
+            {
+                "id": "1e6ecc04-42f3-4118-8cfc-b6bbd8b2105a",
+                "username": "Albert",
+                "message": "Second Comment in Manufacturing 2013"
+            }
+        ],
+        "2014": [
+            {
+                "id": "c15ff0f9-878a-40be-88a5-60d131311e6e",
+                "username": "George",
+                "message": "First comment in Manufacturing 2014"
+            },
+        ],
+    },
+    'Technology': {
+        "2015": [
+            {
+                "id": "8d198202-488c-4fcb-94e4-e9a292479247",
+                "username": "Mike",
+                "message": "First comment in Technology 2015"
+            },
+        ],
+    },
+    'Education': {},
+    'Finance': {},
+    'Agriculture': {},
+}

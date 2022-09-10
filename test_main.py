d2c268d127d0295c77e96cac94ebf7bcf8e256be
einsteinNjoroge <forensicslabnet@gmail.com>
2022-09-09 22:00:11 -0700

    Add tests for creating a new comment

--- test_main.py
+++ test_main.py
@@ -17,3 +17,36 @@ def test_fetch_chart_data():
 def test_fetch_comments():
     response = client.get("/comments")
     assert response.status_code == 200
+
+
+def test_create_new_comment():
+    response = client.post(
+        "/comments",
+        json={
+            "username": "foobar",
+            "message": "Foo Bar",
+            "year": "2014",
+            "industry": "Finance"
+        },
+    )
+    response_data = response.json()
+
+    assert response.status_code == 201
+    assert response_data['message'] == 'Comment created'
+    assert response_data['success'] is True
+
+
+def test_create_new_comment_with_non_existent_industry():
+    response = client.post(
+        "/comments",
+        json={
+            "username": "foobar",
+            "message": "Foo Bar",
+            "year": "2014",
+            "industry": "NON-EXISTENT INDUSTRY"
+        },
+    )
+    response_data = response.json()
+
+    assert response.status_code == 400
+    assert response_data['success'] is False
